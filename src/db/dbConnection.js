94606080104673c5d7867c0cd509ca2eb1f9940f
RobiulMan <robiulislam.bg@gmail.com>
2024-05-11 00:25:55 +0600

    change bd url

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const dbConnection = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        });
-        console.log('Database connected successfully');
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("Database connected successfully");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = dbConnection;
